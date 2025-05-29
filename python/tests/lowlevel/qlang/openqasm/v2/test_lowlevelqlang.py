b842044873556e2d548b38366f43a5b6312e2e05
Martin Klacan <m.klacan@student.vu.nl>
2025-05-29 20:50:35 +0200

    implemented QNot with tests

--- python/tests/lowlevel/qlang/openqasm/v2/test_lowlevelqlang.py
+++ python/tests/lowlevel/qlang/openqasm/v2/test_lowlevelqlang.py
@@ -12,6 +12,9 @@ from hhat_lang.dialects.heather.code.simple_ir_builder.ir import (
 )
 from hhat_lang.low_level.quantum_lang.openqasm.v2.qlang import LowLeveQLang
 
+from hhat_lang.low_level.quantum_lang.openqasm.v2.instructions import QNot
+from hhat_lang.core.code.utils import InstrStatus
+
 
 def test_gen_program_single_empty_redim() -> None:
     code_snippet = """OPENQASM 2.0;
@@ -68,3 +71,37 @@ measure q -> c;
     res = qlang.gen_program()
     print(res)
     # assert res == code_snippet
+
+
+def test_qnot_bool():
+    instr = QNot()
+    instrs, status = instr(idxs=(0,))
+    assert instrs == ("x q[0];",)
+    assert status == InstrStatus.DONE
+
+def test_qnot_u2():
+    instr = QNot()
+    instrs, status = instr(idxs=(0, 1))
+    assert instrs == ("x q[0];", "x q[1];")
+    assert status == InstrStatus.DONE
+
+def test_qnot_u3():
+    instr = QNot()
+    instrs, status = instr(idxs=(0, 1, 2))
+    assert instrs == ("x q[0];", "x q[1];", "x q[2];")
+    assert status == InstrStatus.DONE
+
+def test_qnot_u4():
+    instr = QNot()
+    instrs, status = instr(idxs=(0, 1, 2, 3))
+    assert instrs == ("x q[0];", "x q[1];", "x q[2];", "x q[3];")
+    assert status == InstrStatus.DONE
+
+if __name__ == "__main__":
+    # test_gen_program_single_empty_redim()
+    # test_gen_program_single_q0_redim()
+    test_qnot_bool()
+    test_qnot_u2()
+    test_qnot_u3()
+    test_qnot_u4()
+    print("All tests passed.")
